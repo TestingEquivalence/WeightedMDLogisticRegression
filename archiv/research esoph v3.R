9480f1d48410ef684f68eb6e3ed3d3ff7aa73744
Ostrovski <vladimir_ostrovski@web.de>
2021-06-24 17:52:02 +0200

    work on data

--- archiv/research esoph v3.R
+++ archiv/research esoph v3.R
@@ -10,8 +10,10 @@ df$agegp=factor(df$agegp,ordered = FALSE)
 df$alcgp=factor(df$alcgp,ordered = FALSE)
 df$tobgp=factor(df$tobgp,ordered = FALSE)
 
-df$alcgp=fct_collapse(df$alcgp,over80=c("80-119","120+"))
-df$agegp=fct_collapse(df$agegp,i25_44=c("25-34","35-44"), over65=c("65-74","75+"))
+#df$alcgp=fct_collapse(df$alcgp,over80=c("80-119","120+"))
+df$agegp=fct_collapse(df$agegp,
+                      i25_44=c("25-34","35-44")) 
+                      #over65=c("65-74","75+"))
 df$tobgp=fct_collapse(df$tobgp,under10=c("0-9g/day"), over10=c("20-29","30+","10-19"))
 
 
@@ -26,12 +28,13 @@ frm="p ~ agegp + alcgp+ tobgp"
 
 # using logit regression
 lr <- glm(frm, df, family = binomial("logit"), weights = n)
-lr$min.distance=sqrt(sum((df$p-lr$fitted.values)^2))
+v=(df$p-lr$fitted.values)*weights(lr)/sum(weights(lr))
+lr$min.distance=sqrt(sum(v*v))
 write.result(lr,"lr.csv")
 
 # using minimum distance regression
 set.seed(01012021)
-mdr = min_dst_logit(frm,df,weights=df$n,test = asymptotic, nSimulation = 1000)
+mdr = min_dst_logit(frm,df,weights=df$n,test = tPercentileBootstrap, nSimulation = 1000)
 write.result(mdr,"mdr.csv")
 
 
